9fd4707901b482ffad5c3339d4d2ded0b5b3e379
kian kheirani <kiankheirani@gmail.com>
2024-03-27 16:39:57 +0430

    'add-react-hooke-form'

--- frontend/src/screens/Login.jsx
+++ frontend/src/screens/Login.jsx
@@ -1,7 +1,32 @@
-import { Button, Container, TextField, Box } from "@mui/material";
+import { Button, Container, TextField, Box, Typography } from "@mui/material";
+import "react-toastify/dist/ReactToastify.css";
+
+import { useForm } from "react-hook-form";
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isValid },
+  } = useForm();
+
+  const onSubmit = (data = 0) => {
+    if (isValid) {
+      console.log("done");
+
+      console.log(data);
+
+      return true;
+    }
+  };
+
   return (
-    <Container sx={{ marginTop: "50px" }} component="form" autoComplete="off">
+    <Container
+      component="form"
+      onSubmit={handleSubmit(onSubmit)}
+      sx={{ marginTop: "50px" }}
+      autoComplete="off"
+    >
       <Box
         sx={{
           maxWidth: "500px",
@@ -12,33 +37,68 @@ function Login() {
         }}
       >
         <TextField
-          id="outlined-password-input"
+          InputLabelProps={{ style: { color: "#496989" } }}
+          sx={{
+            "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+              {
+                borderColor: "#496989",
+                // Change border color to red when focused
+              },
+          }}
+          {...register("name", {
+            required: { value: true, message: "Name is required" },
+            minLength: {
+              value: 3,
+              message: "it should have minimim 3 characters",
+            },
+          })}
+          id="outlined"
+          error={false}
           label="Name"
-          type="password"
+          type="text"
           variant="outlined"
           autoComplete="current-password"
-          required
         />
+        <Typography sx={{ textTransform: "capitalize", color: "#E72929" }}>
+          {errors.name?.message}
+        </Typography>
         <TextField
+          InputLabelProps={{ style: { color: "#496989" } }}
+          sx={{
+            "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+              {
+                borderColor: "#496989",
+              },
+          }}
+          {...register("password", {
+            required: { value: true, message: "password is required" },
+            minLength: {
+              value: 10,
+              message: "it should have minimim 3 characters",
+            },
+          })}
           id="outlined-password-input"
           label="Password"
           variant="outlined"
-          required
           type="password"
           autoComplete="current-password"
           helperText={
             <span
               style={{
                 textTransform: "capitalize",
+                fontSize: "12px",
                 color: "#aaa",
               }}
             >
-              it should be between 5 to 8 character
+              password should be between 5 to 8 character
             </span>
           }
         />
-        <Button variant="contained" sx={{ alignSelf: "start" }}>
-          sign in{" "}
+        <Typography sx={{ textTransform: "capitalize", color: "#E72929" }}>
+          {errors.password?.message}
+        </Typography>
+        <Button variant="contained" sx={{ alignSelf: "start" }} type="submit">
+          sign in
         </Button>
       </Box>
     </Container>
